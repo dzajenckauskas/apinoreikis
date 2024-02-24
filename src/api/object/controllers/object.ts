c689c8ea18f17e74c0ee832ad71bc644aeb3993a
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-02-24 11:00:44 +0200

    update list data if url

--- src/api/object/controllers/object.ts
+++ src/api/object/controllers/object.ts
@@ -7,10 +7,37 @@ export default factories.createCoreController('api::object.object', ({ strapi })
     async find(ctx) {
         const sanitizedQueryParams = await this.sanitizeQuery(ctx);
         const { results, pagination } = await strapi.service('api::object.object').find(sanitizedQueryParams);
-        const sanitizedResults = await this.sanitizeOutput(results, ctx);
+
+        const entities = await Promise.all(results.map(async (entity) => {
+            if (entity.url) {
+                try {
+                    const axiosResponse = await axios.request({
+                        method: "GET",
+                        url: entity.url,
+                        headers: {
+                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+                        }
+                    });
+
+                    const $ = cheerio.load(axiosResponse.data);
+                    const bodyData = $('script').text();
+                    const topbroker = JSON.parse(bodyData);
+
+                    return { ...entity, topbroker };
+                } catch (error) {
+                    console.error("Error fetching data from entity URL:", error);
+                    return entity;
+                }
+            } else {
+                return entity;
+            }
+        }));
+
+        const sanitizedResults = await this.sanitizeOutput(entities, ctx);
 
         return this.transformResponse(sanitizedResults, { pagination });
     },
+
     async findOne(ctx) {
         const { id } = ctx.params;
 
@@ -19,7 +46,6 @@ export default factories.createCoreController('api::object.object', ({ strapi })
             populate: ['images']
         });
         if (entity.url) {
-
             const axiosResponse = await axios.request({
                 method: "GET",
                 url: entity.url,
